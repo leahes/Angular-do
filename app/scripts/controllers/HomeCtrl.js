12b67640f433772411bc41abb1fabad23b111733
Leah <leahstuckey@me.com>
2017-12-20 13:01:08 -0500

    12/20 commits

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,34 @@
 (function() {
-    function HomeCtrl(Task) {
-      this.tasks = Task.all;
-    };
+  function HomeCtrl(Task,$scope) {
+    this.tasks = Task.all;
+    this.taskStatus = function (task) {
+      Task.completeTask(task);
+    }
 
-    angular
-        .module('projectName')
-        .controller('HomeCtrl', ['Task', HomeCtrl]);
+    this.isHidden = function(task){
+      return task.created < (moment().seconds() - 60) || task.complete == true
+    };
+// moment date and time look up format.
+    this.addTask = function(messageTitle, messageDescription, taskPriority){
+      if (messageTitle){
+        var newTask = {
+          title: messageTitle,
+          description: messageDescription,
+          priority: taskPriority,
+          created: moment().format(),
+          completed: false
+        };
+        Task.addTask(newTask);
+      }
+      $scope.clearfunction = function(event){
+        event.messageTitle = null;
+        event.messageDescription = null;
+        event.taskPriority = "high";
+      }
+    }
+  }
+
+  angular
+  .module('projectName')
+  .controller('HomeCtrl', ['Task','$scope', HomeCtrl]);
 })();
